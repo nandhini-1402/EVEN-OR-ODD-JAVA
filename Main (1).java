4510566470491c4cf07ffa317386792e1d4eeab0
Nandhini S <140828098+nandhini-1402@users.noreply.github.com>
2024-04-22 20:12:17 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%2==0){
+            System.out.print("Even number");
+        }
+        else{
+            System.out.print("Odd number");
+        }
+    }
+}
